f194ff54d26c05d578c3efbb415f24361f0d4828
Lena Hou <lenahou2013@gmail.com>
2021-08-25 19:43:02 -0400

    log in with google

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,12 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true, length: { minimum: 2 }
     validates :email, presence: true
 
+    def self.create_from_omniauth(auth)
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.username = auth['info']['first_name']
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex(16)
+        end
+    end
     
 end
