75428c072e7b170fc32c9ae7393f676331cabed4
Lena Hou <lenahou2013@gmail.com>
2021-08-27 21:20:37 -0400

    dynamic index_helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,14 @@ class ApplicationController < ActionController::Base
         ((flash[:message] = "You must log in first!") && (redirect_to login_path)) if !current_user
     end
 
+    # dynamic helper methods => @items = Item.all
+    def show_helper
+        class_name = self.get_class_name
+        instance_variable_set("@#{params["controller"].singularize}", class_name.find_by(id: params[:id]))
+      end
+    
+      def get_class_name
+        Object.const_get(params["controller"].titlecase.singularize)
+      end
+
 end
